5d8aedd3f07ed6e8b1bd2f05ea3faa954f5eeefb
JOATZ <136692977+JOATZ@users.noreply.github.com>
2024-02-16 10:02:01 +0200

    Spelling correction. Login test OK

--- nucampsiteServer/models/user.js
+++ nucampsiteServer/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    usename: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     admin: { type: Boolean, default: false }
 })
