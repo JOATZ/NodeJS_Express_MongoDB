96a6b84e004fa872cbcb6a67346aa2e2f011d860
JOATZ <136692977+JOATZ@users.noreply.github.com>
2024-02-19 11:41:19 +0200

    multer and image uplaod test OK

--- nucampsiteServer/authenticate.js
+++ nucampsiteServer/authenticate.js
@@ -37,6 +37,7 @@ exports.jwtPassport = passport.use(
 exports.verifyUser = passport.authenticate('jwt', { session: false })
 
 exports.verifyAdmin = (req, res, next) => {
+    console.log(req.user)
     if (req.user.admin) {
         return next()
     } else {
