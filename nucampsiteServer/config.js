6517c37e5caa45209e6b04fa13b6be9a1bc87193
JOATZ <136692977+JOATZ@users.noreply.github.com>
2024-02-20 08:50:03 +0200

    ignore config

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     secretKey: '12345-67890-09876-54321',
-    mongoUrl: 'mongodb://127.0.0.1:27017/nucampsite'
+    mongoUrl: 'mongodb://127.0.0.1:27017/nucampsite',
+    facebook: {
+        clientId: '3475033146046966',
+        clientSecret: 'f3299bcc478de63cc3d111e7762d7394'
+    }
 }
